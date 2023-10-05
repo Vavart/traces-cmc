5284c5cc0323abe53035d6618aed2c7a26a89765
Maxime <m.sciare@cyi.ac.cy>
2023-10-05 09:42:55 +0200

    POULE

--- connection.js
+++ connection.js
@@ -4,24 +4,28 @@
 
 // Dependencies
 const mysql = require('mysql');
+require('dotenv').config();
 
-
-// Connection to the database
-const con = mysql.createConnection({
-  host: "e-srv-lamp.univ-lemans.fr",
-  user: "",
-  database: "",
-  password: "",
+// Connection to the database (using .env file)
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
 });
 
-con.connect(function(err) {
+
+// Get all transition table and return it
+connection.connect();
+const query = "SELECT * FROM transition";
+
+connection.query(query, (err, rows, fields) => {
   if (err) throw err;
-  console.log("Connected!");
-
-    var sql = "SELECT * FROM transition";
-
-    con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(result);
-    });
+  console.log(rows[0].IDTran);
 });
+
+
+// Need to use JSON.stringify() to convert the object to a string (to be able to send it put it in a json file)
+// error.code
+
+connection.end();
